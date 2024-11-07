02ab9f9e7b5ade6b7827eac469b66318e6a864e5
ralajaji <ralajaji0@gmail.com>
2024-11-08 01:25:14 +0300

    updated

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -46,7 +46,7 @@ export default function LandingPage() {
         <section className="relative min-h-[600px] lg:min-h-[700px] flex items-center">
           <div className="absolute inset-0 overflow-hidden">
             <img 
-              src={import.meta.env.BASE_URL + "/src/assets/soccer-field.jpg" }
+              src={"https://ralajaji.github.io/sport.github.io/src/assets/soccer-field.jpg" }
               alt="Soccer Field" 
               className="w-full h-full object-cover object-center"
             />
