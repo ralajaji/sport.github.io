13d3c5e12968cb57bbbf3a2ead0352a6a61e189a
ralajaji <ralajaji0@gmail.com>
2024-11-08 00:55:58 +0300

    updated

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -8,6 +8,7 @@ import {
   ChevronDownIcon,
 } from '@heroicons/react/24/outline'
 import { useTranslation } from '../contexts/LanguageContext'
+import image from '../assets/soccer-field.jpg'
 
 export default function LandingPage() {
   const { t } = useTranslation()
@@ -46,7 +47,7 @@ export default function LandingPage() {
         <section className="relative min-h-[600px] lg:min-h-[700px] flex items-center">
           <div className="absolute inset-0 overflow-hidden">
             <img 
-              src="/src/assets/soccer-field.jpg" 
+              src={image} 
               alt="Soccer Field" 
               className="w-full h-full object-cover object-center"
             />
